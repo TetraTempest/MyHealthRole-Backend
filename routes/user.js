d149ce1274cfdf39417807bf87db2bce71535ac1
Mohammed Hesham Farhan <moh.hesham.f@gmail.com>
2023-01-18 10:03:24 +0300

    Hospital User Update

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const {
   updateUser,
   deleteUser,
   getUsers,
+  updateHospital,
 } = require("../controllers/user");
 const express = require("express");
 
@@ -12,5 +13,6 @@ userRoute.post("/create/user", createUser);
 userRoute.get("/fetch/users", getUsers);
 userRoute.put("/update/user/:id", updateUser);
 userRoute.delete("/delete/user/:id", deleteUser);
+userRoute.put("/update/user/hospital/:id", updateHospital);
 
 module.exports = userRoute;
